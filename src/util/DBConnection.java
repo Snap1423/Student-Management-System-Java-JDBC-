409c4afe145355c50c975589c05dbb64ae5b348d
Kushal <kushalr170208@gmail.com>
2026-02-07 20:01:15 +0530

    minor changes in DBConnection file

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static String URL = "jdbc:mysql://localhost:3306/your_db_name";
+    private static String URL = "jdbc:mysql://localhost:3306/student_db";
     private static String USERNAME = "root";
     private static String PASSWORD = "your_password";
 
